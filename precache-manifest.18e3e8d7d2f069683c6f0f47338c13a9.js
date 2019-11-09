ac6fb057d237cf0988ed136c8822edc72cc07fa0
Mariano Conti <nanexcool@gmail.com>
2019-11-09 20:19:23 -0300

    Updates

---
+++ precache-manifest.18e3e8d7d2f069683c6f0f47338c13a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4399084f220a74cae2e7881ae89495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fee854a6c70ef33075",
+    "url": "/static/js/2.55460586.chunk.js"
+  },
+  {
+    "revision": "7c82c7468341ca89fa09",
+    "url": "/static/js/main.8c1ad5e1.chunk.js"
+  },
+  {
+    "revision": "32f510d4fdff8ae4fc64",
+    "url": "/static/js/runtime-main.b03e97b4.js"
+  },
+  {
+    "revision": "cfb65a48aebbae19a7d184b06b3900ee",
+    "url": "/static/media/dai.cfb65a48.svg"
+  }
+]);
